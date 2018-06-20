4e852494228f9f4affe8941e9f3b0c56044b7399
Emiton <emiton_alves@my.uri.edu>
2018-06-20 17:26:37 -0400

    Playing around with Ebay site and selenium

---
+++ EbayScrape/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace EbayScrape
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                driver.Navigate().GoToUrl("https://www.ebay.com/");
+
+                IWebElement query = driver.FindElement(By.Name("_nkw"));
+
+                query.SendKeys("xbox one");
+
+                query.Submit();
+
+                var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+                wait.Until(d => d.Title.StartsWith("cheese", StringComparison.OrdinalIgnoreCase));
+
+                Console.WriteLine("Page title is: " + driver.Title);
+            }
+        }
+    }
+}
